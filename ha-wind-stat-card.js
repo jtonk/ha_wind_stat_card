4aecd29be48578e54e61d892beb9561753796432
Jasper Tonk <j.c.t.tonk@gmail.com>
2025-07-10 09:18:35 +0200

    Update ha-wind-stat-card.js

--- ha-wind-stat-card.js
+++ ha-wind-stat-card.js
@@ -302,14 +302,7 @@ class HaWindStatCard extends LitElement {
     }
     .wind-bar-segment {
     }
-    .bar-container{
-      position: relative;
-      width: 100%;
-      display: flex;
-      flex-direction: column-reverse;
-      align-items: stretch;
-      transition: height 0.6s ease;
-    }
+
     .dir-container {
       flex-shrink: 0;
       width: 100%;
